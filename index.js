9e6ff838fce86600b9782a8561bb29a9a0241c08
IrineCKimeto <irenekimetoc@gmail.com>
2024-09-28 16:07:37 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// Write your solution in this file!
+var customerName = 'bob'
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+}
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob';
+}
+const leastFavoriteCustomer = 'some customer';
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'new customer';
+}
